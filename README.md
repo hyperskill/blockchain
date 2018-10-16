53804eed22e905d86148e4f4f87b3babb2da54a3
aaaaaa2493 <aaaaaa2493@yandex.ru>
2018-10-16 22:50:26 +0300

    Setup project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Blockchain
+
+Project: Blockchain
+
+Go to the *src* directory, change *Main.java* and put your *.java* file(s) in it.
---
+++ src/blockchain/Main.java
@@ -0,0 +1,7 @@
+package blockchain;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+    }
+}
